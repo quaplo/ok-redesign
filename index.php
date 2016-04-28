50e68fa21bf498f5c024178f9c77dd0b0696b1df
Gregor Demo <demo.gregor@gmail.com>
2016-04-28 11:23:13 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,305 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+<link href='https://fonts.googleapis.com/css?family=Fjalla+One&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=PT+Serif:400,400italic,700,700italic&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+   
+   
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Brand</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Uvod <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">O nas</a></li>
+        <li><a href="#">Kontakt</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>   
+   
+<div class="bg-green">
+    <div class="container">
+        <div class="n-l-form">
+            Hladam <a>Garzonku</a> v stave <a>po Rekonstrukcii</a> v Lokalite <a>Nove Zamky</a> v cene <a>do 20 000</a> Euro 
+            <br/>
+            <a class="btn btn-lg btn-primary">Zobrazit 31 vysledkov</a>
+        </div>
+    </div>
+</div>   
+
+
+<div class="bg-green-dark">
+    <div class="container italic">
+        <div class="n-l-form-info">
+            <p>Zjednodusili sme pre vas vyhladavanie, zahodili skaredy formular a urobili to lepsie</p>
+            <p>Kliknite na lubovolnu polozku ktora je podciarknuta.</p>
+        </div>
+    </div>
+</div>    
+    
+
+<div class="bg-1">
+    <div class="container section-home">
+        <div class="row">
+            <div class="col-lg-4 right">
+                <h2>Garzonky <span>[12x]</span></h2>
+                <p class="text">Ponúkame na <strong>predaj</strong> neogotický kaštieľ v <a>od 20,200 Eur</a> obci Kolta ( okres Nové Zámky ), ktorý bol postavený koncom 19 storočia. Kaštieľ slúžil ako letné sídlo manželky generála a veľkostatkára Károlya Meszárosa. V architektúre stavby prevláda štýl romantizmu a použili sa neogotické <a>od 20,200 Eur</a> a výtvarné prvky. Stavba predstavuje vidiecke sídlo </p>
+                <div class="tags">
+                    
+                </div>
+                
+                <div class="right m-b">
+                    <a class="btn btn-xs btn-link">Nove zamky </a>
+                    <a class="btn btn-xs btn-link">Brno </a>
+                    <a class="btn btn-xs btn-link">Surany </a>
+                    <a class="btn btn-xs btn-link">Sahy </a>
+                </div>
+                
+                <div class="right">
+                    <a class="btn btn-lg btn-primary center">Zobrazit vsetky  "Garzonky"</a>
+                </div>
+            </div>
+            
+            <div class="col-lg-6  ">
+                <div class="prop-list ">
+                    <img class="prsnt" src="http://nucleusproperties.in/uploads/interior/interior_design_image1423034853.jpg" />
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <span class="bg-f">0<sup>&#8364;</sup></span>
+                            <h4>Zobrazit  Garzonky podla ceny</h4>
+                            <ul class="section-nav">
+                                <li><a href="#">Do 30 000 Eur</a></li>
+                                <li><a href="#">od 30 000 Eur</a></li>
+                            </ul>                             
+                        </div>
+                        <div class="col-lg-6">
+                            <span class="bg-f">m<sup>2</sup></span>
+                            <h4>Zobrazit  Garzonky podla velkosti</h4>
+                            <ul class="section-nav">
+                                <li><a href="#">Do 30 000 Eur</a></li>
+                                <li><a href="#">od 30 000 Eur</a></li>
+                                <li><a href="#">Do 30 000 Eur</a></li>
+                                <li><a href="#">Do 30 000 Eur Us</a></li>
+                            </ul>                              
+                        </div>
+                    </div>
+
+                                       
+                </div>
+               
+            </div>        
+            
+            <div class="col-lg-2">
+                <div class="prop-list bg-2 m-b">
+                    
+                    <img class="prsnt" src="http://4.bp.blogspot.com/-msGd1InSt70/Uywm-E2RroI/AAAAAAAABfk/wunxQFb6g-g/s1600/torino_google-map.png" />
+                    <h4>Lokalita</h4>
+                    <ul class="section-nav">
+                        <li><a href="#">Bratislava</a></li>
+                        <li><a href="#">Bratislava</a></li>
+                        <li><a href="#">Bratislava</a></li>
+                        <li><a href="#">Bratislava Us</a></li>
+                    </ul>    
+                                   
+                </div>
+                <a class="btn btn-lg btn-primary btn-block center">Zobrazit na mape</a>   
+            </div>                   
+        
+            
+        </div>
+    </div>    
+</div>
+
+
+
+
+
+
+<div class="container"><div class="row">
+    <div class="col-lg-3">
+        <img class="prsnt" src="http://ok-reality.sk/files/gallery/thumbs/8904b0b810384e37d05938962a02fa11.jpg" />
+    </div>
+    <div class="col-lg-3">
+        <img class="prsnt" src="http://ok-reality.sk/files/gallery/thumbs/7d4771f85b03ded55dcd6026a07291e2.jpg" />
+    </div>
+    <div class="col-lg-3">
+        <img class="prsnt" src="https://pixabay.com/static/uploads/photo/2015/11/07/21/29/living-1032733_960_720.jpg" />
+    </div>
+    <div class="col-lg-3">
+        <img class="prsnt" src="https://pixabay.com/static/uploads/photo/2015/11/07/21/29/living-1032733_960_720.jpg" />
+    </div>            
+    </div></div>
+
+
+
+
+<div class="bg-1">
+    <div class="container section-home">
+        <div class="row">
+            
+            
+            <div class="col-lg-6">
+                <div id="carousel-example-generic" class="carousel slide m-b" data-ride="carousel">
+                <!-- Indicators -->
+
+
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" role="listbox">
+                    <div class="item active">
+                    <div class="carousel-caption">
+
+                    </div>
+                        <div class="pict carousel-article" >
+                            <h3>
+                                <a>Garzonka - Nove zamky
+                                    </a>
+                                </h3>
+                                <span class="status">Po ciastocnej rekonstrukcii</span>
+                            <span>21580Eur - 62m<sup>2</sup></span>
+                                                    
+                        </div>      
+                    </div>
+                    <div class="item">
+                    <div class="carousel-caption">
+
+                    </div>
+                        <div class="pict carousel-article" >
+                            <h3>
+                                <a>Garzonka - Nove zamky
+                                    </a>
+                                </h3>
+                                <span class="status">Po ciastocnej rekonstrukcii</span>
+                            <span>21580Eur - 62m<sup>2</sup></span>
+                                                    
+                        </div>      
+                    </div>
+                    
+                </div>
+
+                <!-- Controls -->
+                <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+                </div>
+                
+                
+                
+                <div class="row">
+                    
+                    <div class="col-lg-4">
+                        <div class="pics-small">
+                            <i class="glyphicon glyphicon-plus"></i>
+                            <a class="btn">Viac moznosti</a>
+                        </div>
+                    </div>                     
+                    
+                    <div class="col-lg-4">
+                        <div class="pics-small">
+                            <img src="http://ok-reality.sk/files/gallery/thumbs/a846f7c4a59055f4c79d6b8725a33444.jpg" />
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="pics-small">
+                            <img src="http://ok-reality.sk/files/gallery/thumbs/d26051da3fc16de87c206bd2af0d8636.jpg" />
+                        </div>
+                    </div>
+                   
+                </div>
+                
+                
+                
+                
+            </div>            
+            
+            <div class="col-lg-6 left">
+                <h2>Pozemky <span>(8)</span></h2>
+                <p class="text">Ponúkame na <strong>predaj</strong> neogotický kaštieľ v <a>od 20,200 Eur</a> obci Kolta ( okres Nové Zámky ), ktorý bol postavený koncom 19 storočia. Kaštieľ slúžil ako letné sídlo manželky generála a veľkostatkára Károlya Meszárosa. V architektúre stavby prevláda štýl romantizmu a použili sa neogotické <a>od 20,200 Eur</a> a výtvarné prvky. Stavba predstavuje vidiecke sídlo </p>
+                <div class="tags">
+                    
+                </div>
+                
+                <div class=" m-b">
+                    <a class="btn btn-xs btn-link">Nove zamky </a>
+                    <a class="btn btn-xs btn-link">Brno </a>
+                    <a class="btn btn-xs btn-link">Surany </a>
+                    <a class="btn btn-xs btn-link">Sahy </a>
+                </div>
+                
+                <div class="left">
+                    <a class="btn btn-lg btn-primary center">Dostupnost</a>
+                </div>
+            </div>            
+            
+        </div>
+    </div>    
+</div>
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
